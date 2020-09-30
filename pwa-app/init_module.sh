818e5cd88ddd7747e13776945548bbea46eb3476
peekayam <48091471+peekayam@users.noreply.github.com>
2020-09-30 21:55:23 +0530

    Update init_module.sh

--- pwa-app/init_module.sh
+++ pwa-app/init_module.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ $(npm list angular-devkit/build-angular|grep empty -wc)==1 ]
 then
-        `npm install --save-dev @angular-devkit/build-angular`
+        npm install --save-dev @angular-devkit/build-angular
 fi
